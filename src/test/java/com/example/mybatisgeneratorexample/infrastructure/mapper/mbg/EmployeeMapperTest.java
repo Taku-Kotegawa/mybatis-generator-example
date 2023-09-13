9af3ce78b3520e225e7465228f0a7bc3c3089b5a
Taku Kotegawa <tkotegawa@stnet.co.jp>
2023-09-14 07:54:49 +0900

    テストの準備

---
+++ src/test/java/com/example/mybatisgeneratorexample/infrastructure/mapper/mbg/EmployeeMapperTest.java
@@ -0,0 +1,58 @@
+package com.example.mybatisgeneratorexample.infrastructure.mapper.mbg;
+
+import org.junit.jupiter.api.Nested;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+class EmployeeMapperTest {
+
+    @Nested
+    class countByExample {
+    }
+
+    @Nested
+    class updateByPrimaryKey {
+    }
+
+    @Nested
+    class updateByPrimaryKeySelective {
+    }
+
+    @Nested
+    class updateByExample {
+    }
+
+    @Nested
+    class updateByExampleSelective {
+    }
+
+    @Nested
+    class selectByPrimaryKey {
+    }
+
+    @Nested
+    class selectByExample {
+    }
+
+    @Nested
+    class selectByExampleWithRowbounds {
+    }
+
+    @Nested
+    class insertSelective {
+    }
+
+    @Nested
+    class insert {
+    }
+
+    @Nested
+    class deleteByPrimaryKey {
+    }
+
+    @Nested
+    class deleteByExample {
+    }
+}
